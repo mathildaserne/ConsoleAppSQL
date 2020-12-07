7236bab41c3e5796e89f64e9a6699a5876145909
mathilda serneblad <mathilda.serneblad@icloud.com>
2020-12-07 20:20:28 +0100

    SQL

--- ConsoleAppSQL/DoorEventsLog.cs
+++ ConsoleAppSQL/DoorEventsLog.cs
@@ -11,24 +11,28 @@ using System.Data;
 
 class DoorEventsLog:SQL
 {
+        public static int MaxEntries = 20;
         
         public static DataTable FindEntriesByDoor(string Door)
         {
+            //Hämtar data från databasen 
             string DörrKommando = @"SELECT Main.DatumTid, Main.Location, Main.Kod, 
             Main.Tagg, Main.Tenant FROM Main
              
-            JOIN Event On Main.Kod=Event.ID
-            JOIN Location On Main.Location=Location.ID
-            JOIN Tenant On Main.Tenant=Tenant.ID 
-            LEFT JOIN Tenant AS Tagg ON Main.Tagg=Tagg.ID           
-            WHERE Location.Door=@Door";
+            JOIN Event On Main.Kod=Event.Kod
+            JOIN Location On Main.Location=Location.Door
+            JOIN Tenant On (Main.Tenant=Tenant.Person AND Main.Tagg=Tenant.Tagg)          
+            WHERE Location.Door=@Door 
+            LIMIT @Entry";
 
-        DataTable dt = new DataTable();
-            using (SQLiteConnection con = new SQLiteConnection("data source=" + "MinDatabase")) 
+            
+            DataTable dt = new DataTable();
+            using (SQLiteConnection con = new SQLiteConnection("data source=" + "MinDatabase.db")) 
             {
                 con.Open();
                 SQLiteCommand cmd = new SQLiteCommand(DörrKommando,con);
                 cmd.Parameters.AddWithValue("@Door", Door);
+                cmd.Parameters.AddWithValue("@Entry", MaxEntries);
                 SQLiteDataAdapter da = new SQLiteDataAdapter(cmd);
                 da.Fill(dt);
             }
@@ -37,22 +41,23 @@ class DoorEventsLog:SQL
 
         public static DataTable FindEntriesByEvent(string Event)
         {
-
+            //Hämtar data från databasen
             string DörrKommando = @"SELECT Main.DatumTid, Main.Location, Main.Kod, 
             Main.Tagg, Main.Tenant FROM Main
 
-            JOIN Event On Main.Kod=Event.ID
-            JOIN Location On Main.Location=Location.ID
-            JOIN Tenant On Main.Tenant=Tenant.ID 
-            LEFT JOIN Tenant AS Tagg ON Main.Tagg=Tagg.ID           
-            WHERE Event.Kod=@Kod";
+            JOIN Event On Main.Kod=Event.Kod
+            JOIN Location On Main.Location=Location.Door
+            JOIN Tenant On (Main.Tenant=Tenant.Person AND Main.Tagg=Tenant.Tagg)           
+            WHERE Event.Kod=@Kod
+            LIMIT @Entry";
 
             DataTable dt = new DataTable();
-            using (SQLiteConnection con = new SQLiteConnection("data source=" + "MinDatabase"))
+            using (SQLiteConnection con = new SQLiteConnection("data source=" + "MinDatabase.db"))
             {
                 con.Open();
                 SQLiteCommand cmd = new SQLiteCommand(DörrKommando, con);
                 cmd.Parameters.AddWithValue("@Kod", Event);
+                cmd.Parameters.AddWithValue("@Entry", MaxEntries);
                 SQLiteDataAdapter da = new SQLiteDataAdapter(cmd);
                 da.Fill(dt);
             }
@@ -62,22 +67,23 @@ class DoorEventsLog:SQL
 
         public static DataTable FindEntriesByLocation(string Location)
         {
-
+            //Hämtar data från databasen
             string DörrKommando = @"SELECT Main.DatumTid, Main.Location, Main.Kod, 
             Main.Tagg, Main.Tenant FROM Main
              
-            JOIN Event On Main.Kod=Event.ID
-            JOIN Location On Main.Location=Location.ID
-            JOIN Tenant On Main.Tenant=Tenant.ID 
-            LEFT JOIN Tenant AS Tagg ON Main.Tagg=Tagg.ID           
-            WHERE Location.Door LIKE @Door";
+            JOIN Event On Main.Kod=Event.Kod
+            JOIN Location On Main.Location=Location.Door
+            JOIN Tenant On (Main.Tenant=Tenant.Person AND Main.Tagg=Tenant.Tagg)             
+            WHERE Location.Door = @Door
+            LIMIT @Entry";
 
             DataTable dt = new DataTable();
-            using (SQLiteConnection con = new SQLiteConnection("data source=" + "MinDatabase"))
+            using (SQLiteConnection con = new SQLiteConnection("data source=" + "MinDatabase.db"))
             {
                 con.Open();
                 SQLiteCommand cmd = new SQLiteCommand(DörrKommando, con);
-                cmd.Parameters.AddWithValue("@Door", "%"+Location+"%");
+                cmd.Parameters.AddWithValue("@Door", Location);
+                cmd.Parameters.AddWithValue("@Entry", MaxEntries);
                 SQLiteDataAdapter da = new SQLiteDataAdapter(cmd);
                 da.Fill(dt);
             }
@@ -87,22 +93,23 @@ class DoorEventsLog:SQL
 
         public static DataTable FindEntriesByTag(string Tagg)
         {
-
+            //Hämtar data från databasen
             string DörrKommando = @"SELECT Main.DatumTid, Main.Location, Main.Kod, 
             Main.Tagg, Main.Tenant FROM Main
              
-            JOIN Event On Main.Kod=Event.ID
-            JOIN Location On Main.Location=Location.ID
-            JOIN Tenant On Main.Tenant=Tenant.ID 
-            LEFT JOIN Tenant AS Tagg ON Main.Tagg=Tagg.ID           
-            WHERE Tenant.Tagg=@Tagg";
+            JOIN Event On Main.Kod=Event.Kod
+            JOIN Location On Main.Location=Location.Door
+            JOIN Tenant On (Main.Tenant=Tenant.Person AND Main.Tagg=Tenant.Tagg)             
+            WHERE Tenant.Tagg=@Tagg
+            LIMIT @Entry";
 
             DataTable dt = new DataTable();
-            using (SQLiteConnection con = new SQLiteConnection("data source=" + "MinDatabase"))
+            using (SQLiteConnection con = new SQLiteConnection("data source=" + "MinDatabase.db"))
             {
                 con.Open();
                 SQLiteCommand cmd = new SQLiteCommand(DörrKommando, con);
                 cmd.Parameters.AddWithValue("@Tagg", Tagg);
+                cmd.Parameters.AddWithValue("@Entry", MaxEntries);
                 SQLiteDataAdapter da = new SQLiteDataAdapter(cmd);
                 da.Fill(dt);
             }
@@ -112,22 +119,23 @@ class DoorEventsLog:SQL
 
         public static DataTable FindEntriesByTenant(string Tenant)
         {
-
+            //Hämtar data från databasen
             string DörrKommando = @"SELECT Main.DatumTid, Main.Location, Main.Kod, 
             Main.Tagg, Main.Tenant FROM Main
              
-            JOIN Event On Main.Kod=Event.ID
-            JOIN Location On Main.Location=Location.ID
-            JOIN Tenant On Main.Tenant=Tenant.ID 
-            LEFT JOIN Tenant AS Tagg ON Main.Tagg=Tagg.ID           
-            WHERE Tenant.Person=@Person";
+            JOIN Event On Main.Kod=Event.Kod
+            JOIN Location On Main.Location=Location.Door
+            JOIN Tenant On (Main.Tenant=Tenant.Person AND Main.Tagg=Tenant.Tagg)           
+            WHERE Tenant.Person=@Person
+            LIMIT @Entry";
 
             DataTable dt = new DataTable();
-            using (SQLiteConnection con = new SQLiteConnection("data source=" + Database))
+            using (SQLiteConnection con = new SQLiteConnection("data source=" + "MinDatabase.db"))
             {
                 con.Open();
                 SQLiteCommand cmd = new SQLiteCommand(DörrKommando, con);
                 cmd.Parameters.AddWithValue("@Person", Tenant);
+                cmd.Parameters.AddWithValue("@Entry", MaxEntries);
                 SQLiteDataAdapter da = new SQLiteDataAdapter(cmd);
                 da.Fill(dt);
             }
@@ -138,14 +146,19 @@ class DoorEventsLog:SQL
         public static DataTable ListTenantsAt(string LGH)
         {
 
-            string DörrKommando = @" ";
+            string DörrKommando = @"SELECT Main.Location, Main.Tagg, Main.Tenant FROM Main 
+            JOIN Tenant On (Main.Tagg=Tenant.Tagg AND Main.Tenant=Tenant.Person)
+            JOIN Location On Main.Location=Location.Door
+            WHERE Location.Door=@LGHNR 
+            LIMIT @Entry";
 
             DataTable dt = new DataTable();
             using (SQLiteConnection con = new SQLiteConnection("data source=" + "MinDatabase.db"))
             {
                 con.Open();
                 SQLiteCommand cmd = new SQLiteCommand(DörrKommando, con);
-                cmd.Parameters.AddWithValue("@LGH NR", LGH);
+                cmd.Parameters.AddWithValue("@LGHNR", LGH);
+                cmd.Parameters.AddWithValue("@Entry", MaxEntries);
                 SQLiteDataAdapter da = new SQLiteDataAdapter(cmd);
                 da.Fill(dt);
             }
@@ -153,10 +166,26 @@ class DoorEventsLog:SQL
 
         }
 
-        public static void LogEntry()
+        public static DataTable LogEntry(string DateTime, string Loc, string Code, string Tag, string Ten)
         {
 
+            string LoggEntry = @"Insert into main(DatumTid, Location, Kod, Tagg, Tenant) values
+            (@DatumTid, @Location, @Kod, @Tagg, @Tenant)";
+            DataTable dt = new DataTable();
+            using (SQLiteConnection con = new SQLiteConnection("data source=" + "MinDatabase.db"))
+            {
+                con.Open();
+                SQLiteCommand cmd = new SQLiteCommand(LoggEntry, con);
+                cmd.Parameters.AddWithValue("@DatumTid", DateTime);
+                cmd.Parameters.AddWithValue("@Location", Loc);
+                cmd.Parameters.AddWithValue("@Kod", Code);
+                cmd.Parameters.AddWithValue("@Tagg", Tag);
+                cmd.Parameters.AddWithValue("@Tenant", Ten);
+                SQLiteDataAdapter da = new SQLiteDataAdapter(cmd);
+                da.Fill(dt);
 
+            }
+            return dt;
 
         }
 
--- ConsoleAppSQL/Program.cs
+++ ConsoleAppSQL/Program.cs
@@ -12,10 +12,47 @@ namespace ConsoleAppSQL
         static void Main()
         {
             SQL.SkapaDatabas();
-            var byDoor = DoorEventsLog.FindEntriesByDoor("LGH0101")?.Rows;
+
+            var byDoor = DoorEventsLog.FindEntriesByDoor("LGH 0102")?.Rows;
+            var byEvent = DoorEventsLog.FindEntriesByEvent("DÖIN")?.Rows;
+            var byLocation = DoorEventsLog.FindEntriesByLocation("VAKT")?.Rows;
+            var byTag = DoorEventsLog.FindEntriesByTag("0102A")?.Rows;
+            var byTenant = DoorEventsLog.FindEntriesByTenant("Liam Jönsson")?.Rows;
+            var tenants = DoorEventsLog.ListTenantsAt("LGH 0201")?.Rows;
+            
             OutputData("search by door", byDoor);
-            Console.ReadKey();
+            OutputData("search by event", byEvent);
+            OutputData("search by location", byLocation);
+            OutputData("search by tag", byTag);
+            OutputData("search by tenant", byTenant);
+            OutputTenants(tenants);
+
+            EnterLogg();
+
         }
+
+        private static void EnterLogg()
+        {
+            string DateTime = Console.ReadLine();
+            string Location = Console.ReadLine();
+            string Kod = Console.ReadLine();
+            string Tagg = Console.ReadLine();
+            string Tenant = Console.ReadLine();
+
+            DoorEventsLog.LogEntry(DateTime, Location, Kod, Tagg, Tenant);
+        }
+
+        private static void OutputTenants(DataRowCollection Tenants)
+        {
+            Console.WriteLine("Tenants");
+            foreach(DataRow r in Tenants)
+            {
+                Console.WriteLine($"{r["Location"]} {r["Tagg"]} {r["Tenant"]}");
+
+            }
+
+        }
+        
 
         private static void OutputData(string title, DataRowCollection byCode)
         {
